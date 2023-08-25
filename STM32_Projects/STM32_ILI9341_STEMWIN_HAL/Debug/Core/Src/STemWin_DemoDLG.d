db102d6f34f420d210bcd927491261d953f62600
Salih <salihbmmerciyes@gmail.com>
2023-08-25 17:08:24 +0300

    STM32F4 STemWIN Graphic Library with HAL

---
+++ STM32_Projects/STM32_ILI9341_STEMWIN_HAL/Debug/Core/Src/STemWin_DemoDLG.d
@@ -0,0 +1,191 @@
+Core/Src/STemWin_DemoDLG.o: ../Core/Src/STemWin_DemoDLG.c \
+ ../GUI/Inc/DIALOG.h ../GUI/Inc/WM.h ../GUI/Inc/GUI_ConfDefaults.h \
+ ../Core/Inc/GUIConf.h ../GUI/Inc/GUI_Type.h ../GUI/Inc/LCD.h \
+ ../GUI/Inc/Global.h ../GUI/Inc/WM_GUI.h ../GUI/Inc/GUI.h \
+ ../GUI/Inc/GUI_Version.h ../GUI/Inc/BUTTON.h ../GUI/Inc/DIALOG_Intern.h \
+ ../GUI/Inc/WIDGET.h ../GUI/Inc/WM_Intern.h ../GUI/Inc/GUI_Private.h \
+ ../GUI/Inc/LCD_Protected.h ../GUI/Inc/GUI_Debug.h ../GUI/Inc/SCROLLBAR.h \
+ ../GUI/Inc/CALENDAR.h ../GUI/Inc/CHECKBOX.h ../GUI/Inc/CHOOSECOLOR.h \
+ ../GUI/Inc/CHOOSEFILE.h ../GUI/Inc/DROPDOWN.h ../GUI/Inc/LISTBOX.h \
+ ../GUI/Inc/EDIT.h ../GUI/Inc/FRAMEWIN.h ../GUI/Inc/GRAPH.h \
+ ../GUI/Inc/HEADER.h ../GUI/Inc/ICONVIEW.h ../GUI/Inc/IMAGE.h \
+ ../GUI/Inc/LISTVIEW.h ../GUI/Inc/LISTWHEEL.h ../GUI/Inc/MENU.h \
+ ../GUI/Inc/MULTIEDIT.h ../GUI/Inc/MULTIPAGE.h ../GUI/Inc/DIALOG.h \
+ ../GUI/Inc/PROGBAR.h ../GUI/Inc/RADIO.h ../GUI/Inc/SLIDER.h \
+ ../GUI/Inc/SPINBOX.h ../GUI/Inc/SWIPELIST.h ../GUI/Inc/TEXT.h \
+ ../GUI/Inc/TREEVIEW.h ../GUI/Inc/KNOB.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h \
+ ../Core/Inc/stm32f4xx_hal_conf.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h \
+ ../Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ ../Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f407xx.h \
+ ../Drivers/CMSIS/Include/core_cm4.h \
+ ../Drivers/CMSIS/Include/cmsis_version.h \
+ ../Drivers/CMSIS/Include/cmsis_compiler.h \
+ ../Drivers/CMSIS/Include/cmsis_gcc.h \
+ ../Drivers/CMSIS/Include/mpu_armv7.h \
+ ../Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_exti.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_adc.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_adc_ex.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_crc.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h \
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_spi.h
+
+../GUI/Inc/DIALOG.h:
+
+../GUI/Inc/WM.h:
+
+../GUI/Inc/GUI_ConfDefaults.h:
+
+../Core/Inc/GUIConf.h:
+
+../GUI/Inc/GUI_Type.h:
+
+../GUI/Inc/LCD.h:
+
+../GUI/Inc/Global.h:
+
+../GUI/Inc/WM_GUI.h:
+
+../GUI/Inc/GUI.h:
+
+../GUI/Inc/GUI_Version.h:
+
+../GUI/Inc/BUTTON.h:
+
+../GUI/Inc/DIALOG_Intern.h:
+
+../GUI/Inc/WIDGET.h:
+
+../GUI/Inc/WM_Intern.h:
+
+../GUI/Inc/GUI_Private.h:
+
+../GUI/Inc/LCD_Protected.h:
+
+../GUI/Inc/GUI_Debug.h:
+
+../GUI/Inc/SCROLLBAR.h:
+
+../GUI/Inc/CALENDAR.h:
+
+../GUI/Inc/CHECKBOX.h:
+
+../GUI/Inc/CHOOSECOLOR.h:
+
+../GUI/Inc/CHOOSEFILE.h:
+
+../GUI/Inc/DROPDOWN.h:
+
+../GUI/Inc/LISTBOX.h:
+
+../GUI/Inc/EDIT.h:
+
+../GUI/Inc/FRAMEWIN.h:
+
+../GUI/Inc/GRAPH.h:
+
+../GUI/Inc/HEADER.h:
+
+../GUI/Inc/ICONVIEW.h:
+
+../GUI/Inc/IMAGE.h:
+
+../GUI/Inc/LISTVIEW.h:
+
+../GUI/Inc/LISTWHEEL.h:
+
+../GUI/Inc/MENU.h:
+
+../GUI/Inc/MULTIEDIT.h:
+
+../GUI/Inc/MULTIPAGE.h:
+
+../GUI/Inc/DIALOG.h:
+
+../GUI/Inc/PROGBAR.h:
+
+../GUI/Inc/RADIO.h:
+
+../GUI/Inc/SLIDER.h:
+
+../GUI/Inc/SPINBOX.h:
+
+../GUI/Inc/SWIPELIST.h:
+
+../GUI/Inc/TEXT.h:
+
+../GUI/Inc/TREEVIEW.h:
+
+../GUI/Inc/KNOB.h:
+
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h:
+
+../Core/Inc/stm32f4xx_hal_conf.h:
+
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h:
+
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h:
+
+../Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+
+../Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f407xx.h:
+
+../Drivers/CMSIS/Include/core_cm4.h:
+
+../Drivers/CMSIS/Include/cmsis_version.h:
+
+../Drivers/CMSIS/Include/cmsis_compiler.h:
+
+../Drivers/CMSIS/Include/cmsis_gcc.h:
+
+../Drivers/CMSIS/Include/mpu_armv7.h:
+
+../Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+
+../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h:
+
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h:
+
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h:
+
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_exti.h:
+
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h:
+
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h:
+
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h:
+
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_adc.h:
+
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_adc_ex.h:
+
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_crc.h:
+
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h:
+
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h:
+
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h:
+
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h:
+
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h:
+
+../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_spi.h:
